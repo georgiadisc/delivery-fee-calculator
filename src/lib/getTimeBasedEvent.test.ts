6a78bf61688773f87cd4b0831c09e131a6da450a
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-08 01:29:38 +0200

    feat: add isDisabled field to TimeBasedEvent

--- src/lib/getTimeBasedEvent.test.ts
+++ src/lib/getTimeBasedEvent.test.ts
@@ -1,13 +1,42 @@
-import { events, getTimeBasedEvent } from "./getTimeBasedEvent";
+import { getTimeBasedEvent, type EventDictionary } from "./getTimeBasedEvent";
 
 describe("getTimeBasedEvent()", () => {
+  const mockEvents: EventDictionary = {
+    default: { summary: "Default", rate: 1.0 },
+    fridayRush: { summary: "Friday Rush", rate: 1.2 },
+  };
+
   it("returns default event for a non-Friday and non-rush hour time", () => {
     const nonRushDate = new Date("2023-01-03T12:00:00Z"); // Tuesday, 12 PM UTC
-    expect(getTimeBasedEvent(nonRushDate)).toBe(events.default);
+    expect(getTimeBasedEvent(nonRushDate, mockEvents)).toBe(mockEvents.default);
   });
 
   it("returns Friday rush event for a Friday during rush hours", () => {
     const rushFridayDate = new Date("2023-01-06T16:00:00Z"); // Friday, 4 PM UTC
-    expect(getTimeBasedEvent(rushFridayDate)).toBe(events.fridayRush);
+    expect(getTimeBasedEvent(rushFridayDate, mockEvents)).toBe(
+      mockEvents.fridayRush
+    );
+  });
+
+  it("returns default event when Friday rush event is disabled", () => {
+    const rushFridayDate = new Date("2023-01-06T16:00:00Z"); // Friday, 4 PM UTC
+    const disabledEvents = {
+      ...mockEvents,
+      fridayRush: { ...mockEvents.fridayRush, isDisabled: true },
+    };
+    expect(getTimeBasedEvent(rushFridayDate, disabledEvents)).toEqual(
+      mockEvents.default
+    );
+  });
+
+  it("returns default event for a disabled Friday rush event on any day", () => {
+    const disabledEvents = {
+      ...mockEvents,
+      fridayRush: { ...mockEvents.fridayRush, isDisabled: true },
+    };
+    const anyDate = new Date("2023-01-03T12:00:00Z"); // Tuesday, 12 PM UTC
+    expect(getTimeBasedEvent(anyDate, disabledEvents)).toEqual(
+      mockEvents.default
+    );
   });
 });
--- src/lib/getTimeBasedEvent.ts
+++ src/lib/getTimeBasedEvent.ts
@@ -2,18 +2,29 @@ import { isFridayRush } from "./isFridayRush";
 
 /** */
 export type TimeBasedEvent = Readonly<{
-  /** */
+  /** The title of the event. */
   summary: string;
-  /** */
+  /** The rate or multiplier associated with the event. */
   rate: number;
+  /** Indicates whether the event is disabled. */
+  isDisabled?: boolean;
 }>;
 
 /** */
-export const events: Record<string, TimeBasedEvent> = {
+export type EventDictionary = Record<string, TimeBasedEvent>;
+
+/** */
+export const events: EventDictionary = {
   default: { summary: "Default", rate: 1.0 },
   fridayRush: { summary: "Friday Rush", rate: 1.2 },
 } as const;
 
-export function getTimeBasedEvent(date: Date): TimeBasedEvent {
-  return isFridayRush(date) ? events.fridayRush : events.default;
+/** Determines the time-based event for a given date. */
+export function getTimeBasedEvent(
+  date: Date,
+  events: EventDictionary
+): TimeBasedEvent {
+  return isFridayRush(date) && !events.fridayRush.isDisabled
+    ? events.fridayRush
+    : events.default;
 }
