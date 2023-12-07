70c346cf0ea972ef766114687ccdc4a0ba2b57b3
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-07 22:22:11 +0200

    feat: add documentation for isFridayRush

--- src/lib/isFridayRush.ts
+++ src/lib/isFridayRush.ts
@@ -1,4 +1,4 @@
-/** */
+/** The value for each day of the week, based on the en-US locale. */
 const weekDay = {
   Sunday: 0,
   Monday: 1,
@@ -8,20 +8,24 @@ const weekDay = {
   Friday: 5,
   Saturday: 6,
 } as const;
-/** */
+
+/** The starting hour for Friday rush hours. */
 const fridayRushStartHour = 15;
-/** */
+
+/** The ending hour for Friday rush hours. */
 const fridayRushEndHour = 19;
 
+/** Checks if the given day corresponds to Friday. */
 function isFriday(day: number): boolean {
   return day === weekDay.Friday;
 }
 
+/** Checks if the given hours are within the defined Friday rush hours. */
 function isWithinRushHours(hours: number): boolean {
   return hours >= fridayRushStartHour && hours < fridayRushEndHour;
 }
 
-/** */
+/** Checks if the given date and time fall within the defined Friday rush hours. */
 export function isFridayRush(date: Date): boolean {
   return isFriday(date.getUTCDay()) && isWithinRushHours(date.getUTCHours());
 }
