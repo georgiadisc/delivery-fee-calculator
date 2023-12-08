25f4fc38871099d82a5335f398f843fb87e98dba
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-08 18:43:22 +0200

    docs: add docs for getTimeBasedEvent

--- src/lib/getTimeBasedEvent.ts
+++ src/lib/getTimeBasedEvent.ts
@@ -1,6 +1,5 @@
 import { isFridayRush } from "./isFridayRush";
 
-/** */
 export type TimeBasedEvent = {
   /** The title of the event. */
   summary: string;
@@ -10,20 +9,28 @@ export type TimeBasedEvent = {
   isDisabled?: boolean;
 };
 
-/** */
 export type EventDictionary = Record<string, TimeBasedEvent>;
 
-/** */
+/** A dictionary of predefined Wolt events. */
 export const woltEvents: EventDictionary = {
   default: { summary: "Default", rate: 1.0 },
   fridayRush: { summary: "Friday Rush", rate: 1.2 },
 } as const;
 
-/** Determines the time-based event for a given date. */
+/**
+ * Determines the time-based event for a given date.
+ * @param date - The date for which to determine the time-based event.
+ * @param events - Optional parameter for providing a custom dictionary of
+ * events.
+ */
 export function getTimeBasedEvent(
   date: Date,
   events: EventDictionary = woltEvents
 ): TimeBasedEvent {
+  /**
+   * During the Friday rush (3 - 7 PM UTC), the delivery fee (the total fee
+   * including possible surcharges) will be multiplied by 1.2x.
+   */
   return isFridayRush(date) && !events.fridayRush.isDisabled
     ? events.fridayRush
     : events.default;
