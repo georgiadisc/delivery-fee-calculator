537765a8f707ec7ef91e954148bf48773c29fd59
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-08 18:16:04 +0200

    deps: update docs for calculateSmallOrderFee

--- src/lib/calculateSmallOrderFee.ts
+++ src/lib/calculateSmallOrderFee.ts
@@ -2,11 +2,9 @@
 const smallOrderSurchargeThreshold = 10.0;
 
 /**
- * If the cart value is less than the specified threshold, a small order
- * surcharge is added to the delivery price. The surcharge is the difference
- * between the cart value and {@link smallOrderSurchargeThreshold}. For example
- * if the cart value is 8.90€ and the specified threshold is 10€, the surcharge
- * will be 1.10€.
+ * If the cart value is less than 10€, a small order surcharge is added to the
+ * delivery price. The surcharge is the difference between the cart value and
+ * 10€. For example if the cart value is 8.90€, the surcharge will be 1.10€.
  * @param cart - The total value of the items in the cart.
  */
 export function calculateSmallOrderFee(cart: number): number {
