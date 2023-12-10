87c5adfc2c47fda8e220d680fcaf261c50343101
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-10 21:40:22 +0200

    deps: add chakra ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Delivery Fee Calculator | Wolt Delivery",
@@ -17,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
