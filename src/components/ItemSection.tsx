f4289c01a86cda204506c09231cde0397c1247f6
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-13 00:05:24 +0200

    feat: add ItemSection component

---
+++ src/components/ItemSection.tsx
@@ -0,0 +1,20 @@
+import { HookUsage } from "@/components/HookUsage";
+import { SectionHeaderRow } from "@/components/SectionHeaderRow";
+import { FormControl, Stack } from "@chakra-ui/react";
+import { ReceiptLongRounded } from "@mui/icons-material";
+
+export function ItemSection() {
+  return (
+    <FormControl>
+      <Stack spacing={3}>
+        <SectionHeaderRow
+          title="Amount of items"
+          description="The total number of individual items in your order."
+          icon={<ReceiptLongRounded />}
+          htmlFor="items"
+        />
+        <HookUsage />
+      </Stack>
+    </FormControl>
+  );
+}
