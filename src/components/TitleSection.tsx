ba65f81b14cd8d9d2b009dd2ed897f6e1dd45521
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-12 23:34:08 +0200

    feat: add TitleSection component

---
+++ src/components/TitleSection.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button, Heading, VStack, useDisclosure } from "@chakra-ui/react";
+import { InfoRounded } from "@mui/icons-material";
+import { PricingInfoSection } from "./PricingInfoSection";
+
+export function TitleSection() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <VStack alignItems="start" gap={4}>
+      <Heading as="h1" size="lg">
+        Delivery Fee Calculator 📦
+      </Heading>
+      <Button onClick={onOpen} variant="link" alignSelf="start" gap={2}>
+        <InfoRounded />
+        Learn how we calculate the delivery fee
+      </Button>
+      <PricingInfoSection isOpen={isOpen} onClose={onClose} />
+    </VStack>
+  );
+}
