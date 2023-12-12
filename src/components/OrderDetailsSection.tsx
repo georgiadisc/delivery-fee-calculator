8f4e3745b107fa7373120fa257caffbec0cdf7c0
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-13 00:17:20 +0200

    feat: change height of PriceBreakdown component

--- src/components/OrderDetailsSection.tsx
+++ src/components/OrderDetailsSection.tsx
@@ -18,7 +18,7 @@ interface Props {
 
 export function OrderDetailsSection({ deliveryResponse, onClose }: Props) {
   return (
-    <Box height={{ sm: 440, md: "100%" }} borderRadius={24} overflow="hidden">
+    <Box height={{ sm: 560, md: "100%" }} borderRadius={24} overflow="hidden">
       {deliveryResponse ? (
         <PriceBreakdown deliveryResponse={deliveryResponse} onClose={onClose} />
       ) : (
