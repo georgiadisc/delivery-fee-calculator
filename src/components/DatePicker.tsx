0d3dd24bdfa679750d03ec807106c7344fa8de34
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-13 00:23:00 +0200

    feat: add DatePicker component

---
+++ src/components/DatePicker.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { getTimeRange, getWeekStartingFrom, weekDay } from "@/utils/dateUtils";
+import { HStack, Select, VisuallyHidden } from "@chakra-ui/react";
+import { useState, useCallback } from "react";
+
+const dayOptions = Object.entries(
+  getWeekStartingFrom(new Date().getDay(), [weekDay.Sunday])
+).map(([label, value]) => ({ label, value }));
+
+const timeOptions = Object.entries(
+  getTimeRange({ startHour: 12, endHour: 24, rangeInterval: 5 })
+).map(([label, value]) => ({ label, value }));
+
+export function DatePicker() {
+  const [selectedDay, setSelectedDay] = useState<string>();
+  const [selectedTime, setSelectedTime] = useState<string>();
+
+  const handleSelectDayChange = useCallback(
+    (value: string) => setSelectedDay(value),
+    []
+  );
+
+  const handleSelectTimeChange = useCallback(
+    (value: string) => setSelectedTime(value),
+    []
+  );
+
+  return (
+    <HStack>
+      {/* Day Select */}
+      <VisuallyHidden>
+        <label htmlFor="day">Day</label>
+      </VisuallyHidden>
+      <Select
+        id="day"
+        name="day"
+        borderRadius={100}
+        cursor="pointer"
+        value={selectedDay}
+        onChange={(e) => {
+          handleSelectDayChange(e.target.value);
+        }}
+      >
+        {dayOptions.map((option) => {
+          return (
+            <option key={option.label} value={option.value}>
+              {option.label}
+            </option>
+          );
+        })}
+      </Select>
+      {/* Time Select */}
+      <VisuallyHidden>
+        <label htmlFor="time">Time</label>
+      </VisuallyHidden>
+      <Select
+        id="time"
+        name="time"
+        borderRadius={100}
+        cursor="pointer"
+        value={selectedTime}
+        onChange={(e) => {
+          handleSelectTimeChange(e.target.value);
+        }}
+      >
+        {timeOptions.map((option) => {
+          return (
+            <option key={option.label} value={option.value}>
+              {option.label}
+            </option>
+          );
+        })}
+      </Select>
+    </HStack>
+  );
+}
