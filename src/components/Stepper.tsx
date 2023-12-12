aa33230d48adc538cdc00379a1b88061afeda792
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-13 00:21:10 +0200

    feat: add Stepper component

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button, HStack, Input, useNumberInput } from "@chakra-ui/react";
+import { AddRounded, RemoveRounded } from "@mui/icons-material";
+
+export function Stepper() {
+  const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
+    useNumberInput({
+      step: 1,
+      defaultValue: 1,
+      min: 1,
+    });
+
+  const inc = getIncrementButtonProps();
+  const dec = getDecrementButtonProps();
+  const input = getInputProps();
+
+  return (
+    <HStack>
+      <Button
+        {...dec}
+        borderRadius={100}
+        width={10}
+        height={10}
+        borderWidth={1}
+        colorScheme="gray"
+      >
+        <RemoveRounded />
+      </Button>
+      <Input
+        {...input}
+        borderRadius={100}
+        textAlign="center"
+        id="items"
+        name="items"
+      />
+      <Button
+        {...inc}
+        borderRadius={100}
+        width={10}
+        height={10}
+        borderWidth={1}
+        colorScheme="gray"
+      >
+        <AddRounded />
+      </Button>
+    </HStack>
+  );
+}
