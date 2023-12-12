a65b931be7cb0587c65e3c56a99ba1ec9b75dd25
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-12 23:31:07 +0200

    feat: add ButtonRow component

---
+++ src/components/ButtonRow.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@chakra-ui/react";
+
+export function ButtonRow() {
+  return (
+    <Button type="submit" size="lg" borderRadius={100}>
+      Calculate delivery price
+    </Button>
+  );
+}
