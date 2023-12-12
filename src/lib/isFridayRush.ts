0d6624702d3cceb2946f0bcb6a15e2787829e8c1
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-12 17:02:00 +0200

    feat: transfer weekDay and isFriday to their own module

--- src/lib/isFridayRush.ts
+++ src/lib/isFridayRush.ts
@@ -1,24 +1,10 @@
-/** The value for each day of the week, based on the en-US locale. */
-const weekDay = {
-  Sunday: 0,
-  Monday: 1,
-  Tuesday: 2,
-  Wednesday: 3,
-  Thursday: 4,
-  Friday: 5,
-  Saturday: 6,
-} as const;
+import { isFriday } from "@/utils/dateUtils";
 
 /** The starting hour for Friday rush hours. */
 const fridayRushStartHour = 15;
 
 /** The ending hour for Friday rush hours. */
 const fridayRushEndHour = 19;
-
-/** Checks if the given day corresponds to Friday. */
-function isFriday(day: number): boolean {
-  return day === weekDay.Friday;
-}
 
 /** Checks if the given hours are within the defined Friday rush hours. */
 function isWithinRushHours(hours: number): boolean {
