b667e8612b93d74ac9fc2ce6fdf7d00d24d77aac
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-15 22:17:47 +0200

    feat: add tests for getTimeRange function

--- src/utils/dateUtils.test.ts
+++ src/utils/dateUtils.test.ts
@@ -75,12 +75,13 @@ describe("getTimeRange()", () => {
   const mockDate = new Date("2024-01-03T12:00:00Z"); // Wednesday, 12 PM UTC
   jest.useFakeTimers().setSystemTime(mockDate);
 
-  it("", () => {
+  it("returns timestamps with the default interval", () => {
     const result = getTimeRange({
       startTime: { hour: 12, minute: 0 },
       endTime: { hour: 13, minute: 0 },
     });
-    expect(result).toMatchObject([
+
+    expect(result).toEqual([
       "2024-01-03T12:05:00.000Z",
       "2024-01-03T12:10:00.000Z",
       "2024-01-03T12:15:00.000Z",
@@ -92,6 +93,70 @@ describe("getTimeRange()", () => {
       "2024-01-03T12:45:00.000Z",
       "2024-01-03T12:50:00.000Z",
       "2024-01-03T12:55:00.000Z",
+    ]);
+  });
+
+  it("returns timestamps with a custom interval", () => {
+    const result = getTimeRange({
+      startTime: { hour: 12, minute: 0 },
+      endTime: { hour: 13, minute: 0 },
+      rangeInterval: 10,
+    });
+
+    expect(result).toEqual([
+      "2024-01-03T12:10:00.000Z",
+      "2024-01-03T12:20:00.000Z",
+      "2024-01-03T12:30:00.000Z",
+      "2024-01-03T12:40:00.000Z",
+      "2024-01-03T12:50:00.000Z",
+    ]);
+  });
+
+  it("handles startTime and endTime with the same minute", () => {
+    const result = getTimeRange({
+      startTime: { hour: 12, minute: 0 },
+      endTime: { hour: 12, minute: 0 },
+    });
+
+    expect(result).toEqual([]);
+  });
+
+  it("handles startTime and endTime with the same hour", () => {
+    const result = getTimeRange({
+      startTime: { hour: 12, minute: 30 },
+      endTime: { hour: 12, minute: 45 },
+    });
+
+    expect(result).toEqual([
+      "2024-01-03T12:35:00.000Z",
+      "2024-01-03T12:40:00.000Z",
+    ]);
+  });
+
+  it("handles startTime and endTime with different hours and minutes", () => {
+    const result = getTimeRange({
+      startTime: { hour: 12, minute: 15 },
+      endTime: { hour: 13, minute: 45 },
+    });
+
+    expect(result).toEqual([
+      "2024-01-03T12:20:00.000Z",
+      "2024-01-03T12:25:00.000Z",
+      "2024-01-03T12:30:00.000Z",
+      "2024-01-03T12:35:00.000Z",
+      "2024-01-03T12:40:00.000Z",
+      "2024-01-03T12:45:00.000Z",
+      "2024-01-03T12:50:00.000Z",
+      "2024-01-03T12:55:00.000Z",
+      "2024-01-03T13:00:00.000Z",
+      "2024-01-03T13:05:00.000Z",
+      "2024-01-03T13:10:00.000Z",
+      "2024-01-03T13:15:00.000Z",
+      "2024-01-03T13:20:00.000Z",
+      "2024-01-03T13:25:00.000Z",
+      "2024-01-03T13:30:00.000Z",
+      "2024-01-03T13:35:00.000Z",
+      "2024-01-03T13:40:00.000Z",
     ]);
   });
 });
