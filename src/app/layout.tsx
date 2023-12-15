b4e79f2333e5a2893ab9f4360a13d1f16d4cd9bf
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-15 22:31:55 +0200

    chore: add a period

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Providers } from "./providers";
 export const metadata: Metadata = {
   title: "Delivery Fee Calculator | Wolt Delivery",
   description:
-    "Elevate your Wolt experience by effortlessly planning and calculating delivery fees. Discover and get what you want, delivered to your home and office with ease",
+    "Elevate your Wolt experience by effortlessly planning and calculating delivery fees. Discover and get what you want, delivered to your home and office with ease.",
 };
 
 export default function RootLayout({
