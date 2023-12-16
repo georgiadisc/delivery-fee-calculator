86eeadb3c706cfa0d53d462a7101182d2b481ee6
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-12-16 15:48:44 +0200

    docs: remove examples from docs

--- src/lib/calculateItemFee.ts
+++ src/lib/calculateItemFee.ts
@@ -11,12 +11,6 @@ const bulkFee = 1.2;
  * If the number of items is five or more, an additional 50 cent surcharge is
  * added for each item above and including the fifth item. An extra "bulk" fee
  * applies for more than 12 items of 1,20€.
- * * Example 1: If the number of items is 4, no extra surcharge
- * * Example 2: If the number of items is 5, 50 cents surcharge is added
- * * Example 3: If the number of items is 10, 3€ surcharge (6 x 50 cents) is
- * added
- * * Example 4: If the number of items is 13, 5,70€ surcharge is added ((9 * 50
- * cents) + 1,20€)
  * @param items - The number of items for which the fee is calculated.
  */
 export function calculateItemFee(items: number): number {
